48bf315717c800b8293fc5153bccab91051b70bd
marcoweb <marcocarvalho.web@gmail.com>
2021-10-26 01:34:51 +0000

    Variáveis

---
+++ index.js
@@ -0,0 +1,12 @@
+// Comentário de Uma linha
+/* Comentário
+    Com mais de uma linha */
+
+// Variáveis
+var num = 10;
+let num2 = 20;
+const num3 = 30;
+
+var op = num > num2;
+
+console.log("Olá Mundo!!!")
